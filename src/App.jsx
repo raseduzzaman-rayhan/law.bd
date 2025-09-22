c10141e78cf759880bfbcbadb50566eb2b9368f1
Raseduzzaman-Rayhan <raseduzzamanrayhan@gmail.com>
2025-09-22 23:36:21 +0600

    chore: setup Vite React with TailwindCSS, DaisyUI, React Router, React Icons, Recharts, Google Fonts (Poppins, Hind Siliguri), and added site name & logo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <h1>Everything about this website element installed done</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Root/Root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Root = () => {
+    return (
+        <div>
+            <h1 className='text-5xl text-center'>Router Setup Done</h1>
+        </div>
+    );
+};
+
+export default Root;
